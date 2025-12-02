fd3db85953fdb6f71f25f6c8b6cda285b59f4d71
dina_stepanchuk <stepanchukdin23@gmail.com>
2025-12-03 00:10:34 +0200

    Save hw-7

--- js/task-3.js
+++ js/task-3.js
@@ -1,16 +1,10 @@
-function checkForSpam(message) {
-  const lowerCaseMessage = message.toLowerCase();
-  if (lowerCaseMessage.includes('spam') || lowerCaseMessage.includes('sale')) {
-    return true;
+const textInput = document.querySelector('#name-input');
+const spanOutput = document.querySelector('#name-output');
+textInput.addEventListener('input', event => {
+  const name = event.currentTarget.value.trim();
+  if (name === '') {
+    spanOutput.textContent = 'Anonymous';
   } else {
-    return false;
+    spanOutput.textContent = name;
   }
-}
-
-console.log(checkForSpam('Latest technology news')); // false
-console.log(checkForSpam('JavaScript weekly newsletter')); // false
-console.log(checkForSpam('Get best sale offers now!')); // true
-console.log(checkForSpam('Amazing SalE, only tonight!')); // true
-console.log(checkForSpam('Trust me, this is not a spam message')); // true
-console.log(checkForSpam('Get rid of sPaM emails. Our book in on sale!')); // true
-console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
+});
--- js/task-4.js
+++ js/task-4.js
@@ -1,21 +1,16 @@
-function getShippingCost(country) {
-  switch (country) {
-    case 'China':
-      return `Shipping to ${country} will cost ${100} credits`;
-    case 'Chile':
-      return `Shipping to ${country} will cost ${250} credits`;
-    case 'Australia':
-      return `Shipping to ${country} will cost ${170} credits`;
-    case 'Jamaica':
-      return `Shipping to ${country} will cost ${120} credits`;
-    default:
-      return 'Sorry, there is no delivery to your country';
-  }
-}
+const registerForm = document.querySelector('.login-form');
+registerForm.addEventListener('submit', validateForm);
 
-console.log(getShippingCost('Australia')); // "Shipping to Australia will cost 170 credits"
-console.log(getShippingCost('Germany')); // "Sorry, there is no delivery to your country"
-console.log(getShippingCost('China')); // "Shipping to China will cost 100 credits"
-console.log(getShippingCost('Chile')); // "Shipping to Chile will cost 250 credits"
-console.log(getShippingCost('Jamaica')); // "Shipping to Jamaica will cost 120 credits"
-console.log(getShippingCost('Sweden')); // "Sorry, there is no delivery to your country"
+function validateForm(event) {
+  event.preventDefault();
+  const form = event.target;
+  const email = form.elements.email.value.trim();
+  const password = form.elements.password.value.trim();
+
+  if (email === '' || password === '') {
+    return alert('All form fields must be filled in');
+  }
+
+  console.log(`Login: ${email}, Password: ${password}`);
+  form.reset();
+}
